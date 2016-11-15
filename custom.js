4874fde25535b64c30024f39feb003f16a5bf779
Carl <prinscarl.santoso@mitrais.com>
2016-11-15 17:41:21 +0800

    Create Feature 1

--- custom.js
+++ custom.js
@@ -2,5 +2,7 @@ Ini untuk testing GIT
 Yang akan dilaksanakan  
 di Online Lync 
 Pada tanggal 15 November 2016 
-dan ini akan diikuti oleh xx orang peserta 
+dan ini akan diikuti oleh 8 orang peserta 
 dari Mitrais 
+
+Training on 15 November
