219d2ebc7c6186961413ee31a06a854ac8a59923
linachan09 <maria.angelina@mitrais.com>
2016-11-15 16:42:12 +0700

    Test commit

--- custom.js
+++ custom.js
@@ -2,5 +2,7 @@ Ini untuk testing GIT
 Yang akan dilaksanakan  
 di Online Lync 
 Pada tanggal 15 November 2016 
-dan ini akan diikuti oleh xx orang peserta 
+dan ini akan diikuti oleh 10 orang peserta 
 dari Mitrais 
+
+15 November 2016
