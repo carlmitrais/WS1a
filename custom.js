05116dea7fc12b4e7426df186af8e2bea0299b68
linachan09 <maria.angelina@mitrais.com>
2016-11-15 17:05:09 +0700

    Feature Testing

--- custom.js
+++ custom.js
@@ -1,8 +1,6 @@
-Ini untuk testing GIT 
-Yang akan dilaksanakan  
-di Online Lync 
-Pada tanggal 15 November 2016 
-dan ini akan diikuti oleh 10 orang peserta 
-dari Mitrais 
+Testing Git
+By Online Lync 
+For 10 attendees
+From Mitrais
 
 15 November 2016
